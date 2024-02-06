7378200e7676ed3f0ab4dc3e8200ccff2ab2b910
Darron <darronmgordon@gmail.com>
2024-02-06 22:01:29 +0200

    updates

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 
-gsap.to('',)
+
+gsap.from(".box", {
+
+    x: 1920,
+    rotation: 360,
+    backgroundColor: 'blue',
+    scale: 2,
+    ease: "elastic.out(1,0.3)",
+    duration: 10,
+    stagger: 0.5,
+});
+
+document.querySelectorAll(".box").forEach(function (box) {
+    box.addEventListener("click", function () {
+        gsap.to(".box", {
+            duration: 2,
+            opacity: 0,
+            y: -100,
+            stagger: 0.1,
+            ease: "circ.in"
+        });
+    });
+});
